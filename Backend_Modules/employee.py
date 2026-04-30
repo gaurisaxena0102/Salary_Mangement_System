42ad950a5ce90b58265ff3ff24fdc7aad66394c1
Gauri <06gaurisaxena@gmail.com>
2026-04-30 20:33:34 +0530

    Employee module updated with new features

--- Backend_Modules/employee.py
+++ Backend_Modules/employee.py
@@ -2,6 +2,7 @@
 from .db_connection import get_connection
 
 
+# ADD EMPLOYEE FUNCTION FOR ADDING NEW EMPLOYEES IN THE DATABASE
 def Add_employee(emp_id, name, email, phone, EmpType, dept_id):
     conn = get_connection()
     if conn is None:
@@ -25,6 +26,54 @@ def Add_employee(emp_id, name, email, phone, EmpType, dept_id):
         conn.close()
 
 
+# USED TO UPDATE THE PERMANENT EMPLOYEE TABLE AFTER ADDING THE NEW EMPLOYEE
+def add_permanent_employee(emp_id, basic, bonus, pf):
+    conn = get_connection()
+    if conn is None:
+        print("Database Connection failed ")
+        return
+    cursor = conn.cursor()
+    query = """
+    INSERT INTO permanent_employee(employeeId,BasixSalary,bonus,PF)
+    VALUES(%s,%s,%s,%s)
+    """
+    try:
+        cursor.execute(query, (emp_id, basic, bonus, pf))
+        conn.commit()
+        print("Permanent employee details added successfully")
+    except Exception as e:
+        print("Error ", e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+# USED TO UPDATE THE INTERN EMPLOYEE TABLE AFTER ADDING THE NEW EMPLOYEE
+
+
+def add_intern_employee(emp_id, duration):
+    conn = get_connection()
+    if conn is None:
+        print("Database connection failed")
+        return
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO INTERN(EmployeeID, Internship_Duration)
+    VALUES(%s,%s)
+    """
+    try:
+        cursor.execute(query, (emp_id, duration))
+        conn.commit()
+        print("Intern Details Added successfully")
+    except Exception as e:
+        print("Error : ", e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+# THIS FUNCTION WILL DISPLAY THE DATA OF ALL THE EMPLOYEES FROM THE EMPLOYEE TABLE
 def get_Employees():
     conn = get_connection()
     if conn is None:
@@ -52,6 +101,7 @@ def get_Employees():
         conn.close()
 
 
+# THIS FUNCTION WILL DISPLAY THE DATA OF THE EMPLOYEE WHOSE EMPLOYEE_ID WILL BE ENTERED
 def get_employee_by_id(emp_id):
     conn = get_connection()
     if conn is None:
@@ -61,18 +111,23 @@ def get_employee_by_id(emp_id):
     query = "Select * from employee where EmployeeID =%s"
     try:
         cursor.execute(query, (emp_id,))
-        rows = cursor.fetchone()
-        if rows == None:
+        row = cursor.fetchone()
+        if row == None:
             print("Employee not found ")
-        print("------------------------")
-        print("Employee ID -> ", rows[0])
-        print("Name -> ", rows[1])
-        print("Email -> ", rows[2])
-        print("Mobile Number -> ", rows[3])
-        print("Employee Type -> ", rows[4])
-        print("------------------------")
-        print("\n")
+        if row:
 
+            print("------------------------")
+            print("Employee ID -> ", row[0])
+            print("Name -> ", row[1])
+            print("Email -> ", row[2])
+            print("Mobile Number -> ", row[3])
+            print("Employee Type -> ", row[4])
+            print("------------------------")
+            print("\n")
+            return row
+        else:
+            print("Employee not found ")
+            return None
     except Exception as e:
         print("Error : ", e)
     finally:
@@ -80,6 +135,7 @@ def get_employee_by_id(emp_id):
         conn.close()
 
 
+# THIS FUNCTION GIVES THE DATA OF THE EMPLOYEE WHOSE NAME WILL BE ENTERED
 def search_employee_by_name(name):
     conn = get_connection()
     cursor = conn.cursor()
@@ -95,8 +151,7 @@ def search_employee_by_name(name):
             print("No Employee Found")
         else:
             for row in rows:
-                print(
-                    f"""
+                print(f"""
                 ------------------------
                 EmployeeID : {row[0]}
                 Name : {row[1]}
@@ -105,8 +160,7 @@ def search_employee_by_name(name):
                 Type : {row[4]}
                 Department : {row[5]}
                 ------------------------
-                """
-                )
+                """)
                 print("\n")
     except Exception as e:
         print("Error ", e)
@@ -115,6 +169,7 @@ def search_employee_by_name(name):
         conn.close()
 
 
+# THIS GIVES THE TOTAL NUMBER OF EMPLOYEES IN A SPECIFIC DEPARTMENT
 def count_employees(dept_ID):
     conn = get_connection()
     cursor = conn.cursor()
@@ -131,14 +186,116 @@ def count_employees(dept_ID):
     conn.close()
 
 
+# THIS FUNCTION DELETES THE EMPLOYEE DATA
 def delete_employee(emp_id):
     conn = get_connection()
+    if conn is None:
+        print("Databse connection failed ")
+        return
     cursor = conn.cursor()
     query = "Delete from employee where employeeID=%s"
     try:
         cursor.execute(query, (emp_id,))
         conn.commit()
-        print("Employee Deleted")
+        if cursor.rowcount == 0:
+            print("Employee not found")
+        else:
+            print("Employee and Related data deleted Successfully")
+
+    except Exception as e:
+        print("Error : ", e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+# THIS FUNCTION IS USED TO UPDATE THE EMPLOYEE'S PHONE NUMBER OR DEPARTMENT
+def update_employee(emp_id):
+    conn = get_connection()
+    if conn is None:
+        print("Connection Failed ")
+        return
+    cursor = conn.cursor()
+    print("\nWhat do you want to update?")
+    print("1 -> Phone Number")
+    print("2 -> Department")
+    try:
+        choice = int(input("Enter choice :"))
+        if choice == 1:
+            new_phone = int(input("Enter new Phone Number : "))
+            query = "UPDATE Empliyee set phone = %s where EmployeeID=%s"
+            cursor.execute(query, (new_phone, emp_id))
+        elif choice == 2:
+            print("Department Id ----- Department_Name ")
+            print(" 1                        HR")
+            print(" 2                        IT")
+            print(" 3                        Finance")
+            print(" 4                        Marketing")
+            print(" 5                        Production")
+            new_dept = int(input("Enter new Department ID : "))
+            query = "UPDATE Employee Set DepartmentID =%s Where EmployeeID=%s"
+            cursor.execute(query, (new_dept, emp_id))
+        else:
+            print("Invalid Choice ")
+            return
+        conn.commit()
+
+        if cursor.rowcount == 0:
+            print("Employee not found")
+        else:
+            print("Employee updated successfully")
+
+    except Exception as e:
+        print(
+            "Error : ",
+        )
+    finally:
+        cursor.close()
+        conn.close()
+
+
+# THIS FUNCTION WILL WILL ASK FOR CONFIRMATION FROM THE USER BEFORE UPDATING THE EMPLOYEE
+def confirm_and_Update_employee():
+    emp_id = int(input("enter Employee ID to update : "))
+
+    # show employee details
+    employee = get_employee_by_id(emp_id)
+    if not employee:
+        return
+    confirm = input(
+        f"\nDo you want to update {employee[1]}(ID : {employee[0]})? (yes/NO) : "
+    )
+    if confirm.lower() != "yes":
+        print("Update cancelled ")
+        return
+    update_employee(emp_id)
+
+
+# THIS FUNCTION WILL UPDATE THE SALARY FOR A CURRENT EMPLOYEE
+def update_Salary(emp_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    try:
+        print("\nEnter new Salary Details : ")
+
+        basic = float(input("Basic : "))
+        hra = float(input("HRA : "))
+        da = float(input("DA : "))
+        bonus = float(input("Bonus : "))
+        tax = float(input("Tax : "))
+        pf = float(input("PF : "))
+
+        query = """
+        UPDATE SALARY 
+        set BasicSalary=%s, HRA=%s, Bonus=%s,Tax=%s,PF=%s
+        where EmployeeID=%s
+        """
+        cursor.execute(query, (basic, hra, da, bonus, tax, pf, emp_id))
+        conn.commit()
+        if cursor.rowcount == 0:
+            print("Salary record not found ")
+        else:
+            print("Salary Updated successfully")
     except Exception as e:
         print("Error : ", e)
     finally:
