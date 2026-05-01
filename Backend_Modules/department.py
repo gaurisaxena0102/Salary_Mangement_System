6fd8d14f9535d7afb6a154c281845591b4a14abd
Gauri <06gaurisaxena@gmail.com>
2026-05-01 17:00:15 +0530

    Added 2 new features

--- Backend_Modules/department.py
+++ Backend_Modules/department.py
@@ -23,6 +23,9 @@ def add_department(dept_id, dept_name):
 
 def get_departments():
     conn = get_connection()
+    if conn is None:
+        print("Database Connection Failed")
+        return
     cursor = conn.cursor()
     query = "Select* from department"
     try:
@@ -42,6 +45,9 @@ def get_departments():
 
 def get_department_by_id(dept_id):
     conn = get_connection()
+    if conn is None:
+        print("Database Connection Failed")
+        return
     cursor = conn.cursor()
 
     query = "Select * from department where departmentID = %s"
@@ -61,6 +67,9 @@ def get_department_by_id(dept_id):
 
 def count_departments():
     conn = get_connection()
+    if conn is None:
+        print("Database Connection Failed")
+        return
     cursor = conn.cursor()
 
     query = "Select count(*) from department"
@@ -77,6 +86,9 @@ def count_departments():
 
 def show_department_employees(dept_id):
     conn = get_connection()
+    if conn is None:
+        print("Database Connection Failed")
+        return
     cursor = conn.cursor()
 
     query = "Select* from employee where departmentID=%s"
@@ -89,6 +101,71 @@ def show_department_employees(dept_id):
         else:
             for row in rows:
                 print(row)
+    except Exception as e:
+        print("Error : ", e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def employee_count_per_department():
+    conn = get_connection()
+    if conn is None:
+        print("Database Connection Failed")
+        return
+    cursor = conn.cursor()
+    query = """
+    Select d.departmentName, count(e.employeeID) AS TotalEmployees
+    From department d
+    LEFT JOIN employee e ON d.departmentID = e.departmentID
+    Group by departmentName
+    """
+    try:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        if not rows:
+            print("No Departments found. ")
+        else:
+            print("\n--- Employee Count per Department ---")
+            for row in rows:
+                print(f"{row[0]} -> {row[1]}")
+    except Exception as e:
+        print("Error ", e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def department_salary_report():
+    conn = get_connection()
+    if conn is None:
+        print("Database Connection Failed")
+        return
+    cursor = conn.cursor()
+    query = """
+    SELECT d.departmentName,
+    count(e.EmployeeID) AS ToatalEmployees,
+    SUM(s.BasicSalary + s .HRA + s.DA + s.Bonus - s.Tax - s.PF) AS TotalSalary,
+    AVG(s.BasicSalary + s .HRA + s.DA + s.Bonus - s.Tax - s.PF) AS AvgSalary
+    FROM Department d
+    Left Join Employee e on d.departmentID=e.departmentID
+    Left Join Salary s on e.employeeID =s.EmployeeID
+    GROUP BY d.departmentName
+    """
+    try:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        if not rows:
+            print("No data found ")
+        else:
+            print("\n--- Department Salary Report ---")
+            for row in rows:
+                print("Department : ", row[0])
+                print("Total Employees : ", row[1])
+                print("Total Salary : ", float(row[2]) if row[2] else 0)
+                print("Average Salary : ", float(row[3]) if row[3] else 0)
+                print("-----------------------------")
+
     except Exception as e:
         print("Error : ", e)
     finally:
